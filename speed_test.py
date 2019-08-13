59ccc1bb8a5db131ba91b863d2887c2cc9f0cd9b
jeff Teeters <jeff@teeters.us>
2019-08-12 23:55:36 -0700

    add interactive option

--- speed_test.py
+++ speed_test.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 import re
+import readline
 
 java_tool_dir = "/Users/jt/crcns/projects/petr_nwbqe_paper/NwbQueryEngine2"
 python_tools_dir = os.getcwd()
@@ -12,7 +13,15 @@ data_dir = "../sample_data"
 java_cmd=("java -Djava.library.path=src/main/resources/" 
 		" -jar target/nwbqueryengine-1.0-SNAPSHOT-jar-with-dependencies.jar " + data_dir)
 
-query_lines = """
+
+# Queries currently in paper:
+# A) epochs:(start_time>200  &  stop_time<400  |  stop_time>1600)
+# B) */data:  (unit  ==  "unknown")
+# C)/general/subject: (subject_id == "anm00210863") & */epochs/*: (start_time > 500 & start_time < 550 &tags LIKE "%LickEarly%")
+# D)units: (id > -1 & location == "CA3" & quality > 0.8)
+# E)/general:(virusLIKE "%infectionLocation: M2%")
+
+default_queries = """
 # A
 epochs*:(start_time>200 & stop_time<400 | stop_time>1600)
 # B
@@ -36,11 +45,41 @@ tool_info = {"java": {"cmd": java_cmd, "dir": java_tool_dir},
 	"query_index": {"cmd": "python query_index.py -", "dir": python_tools_dir}
 }
 
-def test_speed():
-	global tools, tool_info, query_lines
+def run_query(query):
+	# run query on all three tools, returns times as a triple (one for each tool)
+	global tools, tool_info
+	time_pattern = b"\nreal\t(\d+)m(\d+\.\d+)s\nuser\t(\d+)m(\d+\.\d+)s\nsys\t(\d+)m(\d+\.\d+)s\n"
+	times = []
+	for tool in tools:
+		cmd = tool_info[tool]["cmd"] + " '" + query + "'"
+		tdir = tool_info[tool]["dir"]
+		os.chdir(tdir)
+		print("Starting: ** %s ** with: %s" % (tool, query,) )
+		print("dir:%s" % os.getcwd())
+		print("cmd:%s" % cmd)
+		p = subprocess.run("time " + cmd, shell=True, capture_output=True)
+		# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output_lines = p.stdout.decode("utf-8").split(sep='\n')
+		print("\n".join(output_lines))  # ls output
+		time_output = p.stderr # .decode("utf-8")
+		m = re.fullmatch(time_pattern, time_output)
+		if not m:
+			print("time_output did not match pattern:")
+			print(time_output)
+			print(time_pattern)
+			sys.exit("quitting")
+		time_user = int(m.group(3)) * 60.0 + float(m.group(4))
+		time_sys = int(m.group(5)) * 60.0 + float(m.group(6))
+		total_time = time_user + time_sys
+		print (time_output)
+		print("total time is: %gs" % total_time)
+		times.append(total_time)
+	return times
+
+def run_default_queries():
+	global query_lines
 	results = []
 	results.append(tools)  # heading of output table
-	time_pattern = b"\nreal\t(\d+)m(\d+\.\d+)s\nuser\t(\d+)m(\d+\.\d+)s\nsys\t(\d+)m(\d+\.\d+)s\n"
 	queries = []  # for storing actual queries (without blank lines or comments)
 	alpha = "ABCDEFGHIJKLMNOP"
 	for line in query_lines.split("\n"):
@@ -48,33 +87,11 @@ def test_speed():
 			# skip blanks lines or comments
 			print(line)
 			continue
-		print("\n------- query %s -------\n%s" % (alpha[len(queries)], line))
+		query_code = alpha[len(queries)]
+		print("\n------- query %s -------\n%s" % (query_code, line))
 		query = line
 		queries.append(query)
-		times = []
-		for tool in tools:
-			cmd = tool_info[tool]["cmd"] + " '" + query + "'"
-			tdir = tool_info[tool]["dir"]
-			os.chdir(tdir)
-			print("dir:%s" % os.getcwd())
-			print("cmd:%s" % cmd)
-			p = subprocess.run("time " + cmd, shell=True, capture_output=True)
-			# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			output_lines = p.stdout.decode("utf-8").split(sep='\n')
-			print("\n".join(output_lines))  # ls output
-			time_output = p.stderr # .decode("utf-8")
-			m = re.fullmatch(time_pattern, time_output)
-			if not m:
-				print("time_output did not match pattern:")
-				print(time_output)
-				print(time_pattern)
-				sys.exit("quitting")
-			time_user = int(m.group(3)) * 60.0 + float(m.group(4))
-			time_sys = int(m.group(5)) * 60.0 + float(m.group(6))
-			total_time = time_user + time_sys
-			print (time_output)
-			print("total time is: %gs" % total_time)
-			times.append(total_time)
+		times = run_query(query)
 		results.append(times)
 	# display final timing results
 	print("Queries in test:")
@@ -84,6 +101,58 @@ def test_speed():
 	print("\t".join(["qid",] + results[0]))
 	for i in range(1,len(results)):
 		print("%s\t%g\t%g\t%g" % (alpha[i-1], results[i][0], results[i][1], results[i][2]))
+
+
+# def test_speed_old():
+# 	global tools, tool_info, query_lines
+# 	results = []
+# 	results.append(tools)  # heading of output table
+# 	time_pattern = b"\nreal\t(\d+)m(\d+\.\d+)s\nuser\t(\d+)m(\d+\.\d+)s\nsys\t(\d+)m(\d+\.\d+)s\n"
+# 	queries = []  # for storing actual queries (without blank lines or comments)
+# 	alpha = "ABCDEFGHIJKLMNOP"
+# 	for line in query_lines.split("\n"):
+# 		if line == "" or line[0] == "#":
+# 			# skip blanks lines or comments
+# 			print(line)
+# 			continue
+# 		query_code = alpha[len(queries)]
+# 		print("\n------- query %s -------\n%s" % (alpha[len(queries)], line))
+# 		query = line
+# 		queries.append(query)
+# 		times = []
+# 		for tool in tools:
+# 			cmd = tool_info[tool]["cmd"] + " '" + query + "'"
+# 			tdir = tool_info[tool]["dir"]
+# 			os.chdir(tdir)
+# 			print("Starting: ** %s ** with %s) %s" % (tool, query,) )
+# 			print("dir:%s" % os.getcwd())
+# 			print("cmd:%s" % cmd)
+# 			p = subprocess.run("time " + cmd, shell=True, capture_output=True)
+# 			# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# 			output_lines = p.stdout.decode("utf-8").split(sep='\n')
+# 			print("\n".join(output_lines))  # ls output
+# 			time_output = p.stderr # .decode("utf-8")
+# 			m = re.fullmatch(time_pattern, time_output)
+# 			if not m:
+# 				print("time_output did not match pattern:")
+# 				print(time_output)
+# 				print(time_pattern)
+# 				sys.exit("quitting")
+# 			time_user = int(m.group(3)) * 60.0 + float(m.group(4))
+# 			time_sys = int(m.group(5)) * 60.0 + float(m.group(6))
+# 			total_time = time_user + time_sys
+# 			print (time_output)
+# 			print("total time is: %gs" % total_time)
+# 			times.append(total_time)
+# 		results.append(times)
+# 	# display final timing results
+# 	print("Queries in test:")
+# 	for i in range(len(queries)):
+# 		print("%s. %s" % (alpha[i], queries[i]))
+# 	print("timing results are:")
+# 	print("\t".join(["qid",] + results[0]))
+# 	for i in range(1,len(results)):
+# 		print("%s\t%g\t%g\t%g" % (alpha[i-1], results[i][0], results[i][1], results[i][2]))
 
 
 			# print(time_output)
@@ -98,9 +167,42 @@ def test_speed():
 #    echo $result
 #done <<< "$queries"
 
+def run_single_query(query):
+	global tools
+	times = run_query(query)
+	print("\t".join(tools) + "\n" + "\t".join(map(str, times)))
+
+def do_interactive_queries():
+	print("Enter query, control-d to quit")
+	while True:
+		try:
+			query=input("> ")
+		except EOFError:
+			break;
+		run_single_query(query)
+	print("\nDone running all queries")
+
+
+def display_instructions():
+	print("Usage: %s ( i | - | <query> )" % sys.argv[0])
+	print(" Either one characer: 'i' - interactive mode, '-' use stored default queries")
+	print(" *OR* a single query to execute; must be quoted.")
+	sys.exit("")
+
 
 def main():
-	test_speed()
+	arglen = len(sys.argv)
+	if arglen != 2:
+		display_instructions()
+	if sys.argv[1] == "i":
+		do_interactive_queries()
+	elif sys.argv[1] == "=":
+		run_default_queries()
+	elif sys.argv[1][0] not in ('"', "'"):
+		display_instructions()
+	query = sys.argv[1]
+	run_single_query(query)
+
 
 if __name__ == "__main__":
 	main()
