ddf9a6641c4f92fa8b36cc90040e073db2b603b4
jeff Teeters <jeff@teeters.us>
2019-10-21 11:52:29 -0700

    added small test_data files, modified run_tests to use them

--- run_tests.py
+++ run_tests.py
@@ -27,7 +27,7 @@ def check_sample_files():
 	global test_data_dir
 	if not os.path.isdir(test_data_dir):
 		abort("Directory '%s' not found." % test_data_dir)
-	test_files = ["EC9_B15.nwb", "GP31_B65.nwb"]
+	test_files = ["ecephys_example.nwb", "ophys_example.nwb"]
 	for file in test_files:
 		path = os.path.join(test_data_dir, file)
 		if not os.path.isfile(path):
@@ -54,33 +54,32 @@ def check_build_index():
 	cmd = "python " + os.path.join("..", "build_index.py") + " ./"
 	expected_output = """Creating database 'nwb_index.db'
 scanning directory ./
-Scanning file 1: ./EC9_B15.nwb
-Warning: not indexing string array because too large (packed size = 12287, limit = 10000), at '/general/extracellular_ephys/electrodes/group'
-Scanning file 2: ./GP31_B65.nwb
-Warning: not indexing string array because too large (packed size = 12543, limit = 10000), at '/general/extracellular_ephys/electrodes/group'
+Scanning file 1: ./ecephys_example.nwb
+Scanning file 2: ./ophys_example.nwb
 """
 	run_command(cmd, expected_output)
 
-query1 = '"general/subject: subject_id == \'EC9\'"'
-query2 = '"general/subject: subject_id LIKE \'GP31%\'"'
+query1 = '"general/optophysiology/*: excitation_lambda == 600.0"'
+query2 = '"general/extracellular_ephys/tetrode1: location LIKE \'%hippocampus\'"'
 
 def check_query_index():
 	global query1, query2
 	cmd = "python " + os.path.join("..", "query_index.py") + ' - ' + query1
 	expected_output = """Opening 'nwb_index.db'
 Found 1 matching files:
-[   {   'file': './EC9_B15.nwb',
-        'subqueries': [   [   {   'node': '/general/subject',
-                                  'vind': {'subject_id': 'EC9'},
+[   {   'file': './ophys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/optophysiology/my_imgpln',
+                                  'vind': {'excitation_lambda': 600.0},
                                   'vtbl': {}}]]}]
 """
 	run_command(cmd, expected_output)
 	cmd = "python " + os.path.join("..", "query_index.py") + ' - ' + query2
 	expected_output = """Opening 'nwb_index.db'
 Found 1 matching files:
-[   {   'file': './GP31_B65.nwb',
-        'subqueries': [   [   {   'node': '/general/subject',
-                                  'vind': {'subject_id': 'GP31'},
+[   {   'file': './ecephys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/extracellular_ephys/tetrode1',
+                                  'vind': {   'location': 'somewhere in the '
+                                                          'hippocampus'},
                                   'vtbl': {}}]]}]
 """
 	run_command(cmd, expected_output)
@@ -90,18 +89,19 @@ def check_search_nwb():
 	# cmd = "python " + os.path.join("..", "search_nwb.py") + " " + os.path.join("..", "test_data") + " " + query1
 	cmd = "python " + os.path.join("..", "search_nwb.py") + " ./ " + query1
 	expected_output = """Found 1 matching files:
-[   {   'file': './EC9_B15.nwb',
-        'subqueries': [   [   {   'node': '/general/subject',
-                                  'vind': {'subject_id': 'EC9'},
+[   {   'file': './ophys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/optophysiology/my_imgpln',
+                                  'vind': {'excitation_lambda': 600.0},
                                   'vtbl': {}}]]}]
 """
 	run_command(cmd, expected_output)
 	# cmd = "python " + os.path.join("..", "search_nwb.py") + " " + os.path.join("..", "test_data") + " " + query2
 	cmd = "python " + os.path.join("..", "search_nwb.py") + " ./ " + query2
 	expected_output = """Found 1 matching files:
-[   {   'file': './GP31_B65.nwb',
-        'subqueries': [   [   {   'node': '/general/subject',
-                                  'vind': {'subject_id': 'GP31'},
+[   {   'file': './ecephys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/extracellular_ephys/tetrode1',
+                                  'vind': {   'location': 'somewhere in the '
+                                                          'hippocampus'},
                                   'vtbl': {}}]]}]
 """
 	run_command(cmd, expected_output)
