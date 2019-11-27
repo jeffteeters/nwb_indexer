a0aa9cc19603c96620ff1688889ed3b8b5932de3
jeff Teeters <jeff@teeters.us>
2019-11-26 16:30:03 -0800

    added pytest nwbindexer/test/test_all.py, move data files to new nwbindexer/test directory

---
+++ nwbindexer/test/test_all.py
@@ -0,0 +1,119 @@
+# pytest for nwbindexer installation
+# tests to make sure sample data files are available, then
+# tests commands: build_index, query_index and search_nwb (four tests total)
+
+import os
+import pytest
+import sys
+import subprocess
+import shutil
+
+
+test_data_dir = "./"
+index_file = "nwb_index.db"
+cwd = os.getcwd()
+
+# create temporary directory to use for storing test index file.  Based on:
+# https://stackoverflow.com/questions/25525202/py-test-temporary-folder-for-the-session-scope
+# https://stackoverflow.com/questions/51593595/pytest-auto-delete-temporary-directory-created-with-tmpdir-factory
+@pytest.fixture(scope='module')
+def tmpdir_module(tmpdir_factory):
+	"""A tmpdir fixture for the module scope. Persists throughout the module."""
+	my_tmpdir = tmpdir_factory.mktemp("tmpdir")
+	yield my_tmpdir 
+	shutil.rmtree(str(my_tmpdir))
+
+def test_check_sample_files():
+	# make sure can access sample files
+	global test_data_dir
+	assert os.path.isdir(test_data_dir)
+	test_files = ["ecephys_example.nwb", "ophys_example.nwb"]
+	for file in test_files:
+		path = os.path.join(test_data_dir, file)
+		assert os.path.isfile(path)
+
+def run_command(cmd):
+	print("> %s" % cmd)
+	p = subprocess.run(cmd, shell=True, capture_output=True)
+	output = p.stdout.decode("utf-8").replace("\r", "")  # strip ^M characters (line return) from windows output
+	return output
+	# if output != expected_output:
+	# 	print("Following command failed:\n%s" % cmd)
+	# 	print("Expected:\n%s" % expected_output)
+	# 	print("Found:\n%s" % output)
+	# 	abort()
+
+def test_build_index(tmpdir_module):
+	global test_data_dir, index_file
+	os.chdir(test_data_dir)
+	index_file_path = os.path.join(tmpdir_module, index_file)
+	# index file should not exist
+	assert not os.path.isfile(index_file_path)
+	# cmd = "python " + os.path.join("..", "build_index.py") + " " + os.path.join("..", "test_data")
+	# cmd = "python " + os.path.join("..", "build_index.py") + " ./"
+
+	cmd = "python -m nwbindexer.build_index ./ %s" % index_file_path
+	expected_output = """Creating database '%s'
+scanning directory ./
+Scanning file 1: ./ecephys_example.nwb
+Scanning file 2: ./ophys_example.nwb
+""" % index_file_path
+	output = run_command(cmd)
+	assert output == expected_output
+
+
+query1 = '"general/optophysiology/*: excitation_lambda == 600.0"'
+query2 = '"general/extracellular_ephys/tetrode1: location LIKE \'%hippocampus\'"'
+
+
+def test_query_index(tmpdir_module):
+	global query1, query2
+	index_file_path = os.path.join(tmpdir_module, index_file)
+	cmd = "python -m nwbindexer.query_index %s %s" % (index_file_path, query1)
+	expected_output = """Opening '%s'
+Found 1 matching files:
+[   {   'file': './ophys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/optophysiology/my_imgpln',
+                                  'vind': {'excitation_lambda': 600.0},
+                                  'vtbl': {}}]]}]
+""" % index_file_path
+	output = run_command(cmd)
+	assert output == expected_output
+	cmd = "python -m nwbindexer.query_index %s %s" % (index_file_path, query2)
+	expected_output = """Opening '%s'
+Found 1 matching files:
+[   {   'file': './ecephys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/extracellular_ephys/tetrode1',
+                                  'vind': {   'location': 'somewhere in the '
+                                                          'hippocampus'},
+                                  'vtbl': {}}]]}]
+""" % index_file_path
+	output = run_command(cmd)
+	assert output == expected_output
+
+
+def test_search_nwb(tmpdir_module):
+	index_file_path = os.path.join(tmpdir_module, index_file)
+	global query1, query2
+	# cmd = "python " + os.path.join("..", "search_nwb.py") + " " + os.path.join("..", "test_data") + " " + query1
+	cmd = "python -m nwbindexer.search_nwb ./ %s" % query1
+	expected_output = """Found 1 matching files:
+[   {   'file': './ophys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/optophysiology/my_imgpln',
+                                  'vind': {'excitation_lambda': 600.0},
+                                  'vtbl': {}}]]}]
+"""
+	output = run_command(cmd)
+	assert output == expected_output
+	# cmd = "python " + os.path.join("..", "search_nwb.py") + " " + os.path.join("..", "test_data") + " " + query2
+	cmd = "python -m nwbindexer.search_nwb ./ %s" % query2
+	expected_output = """Found 1 matching files:
+[   {   'file': './ecephys_example.nwb',
+        'subqueries': [   [   {   'node': '/general/extracellular_ephys/tetrode1',
+                                  'vind': {   'location': 'somewhere in the '
+                                                          'hippocampus'},
+                                  'vtbl': {}}]]}]
+"""
+	output = run_command(cmd)
+	assert output == expected_output
+
