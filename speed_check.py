2b701f255e7ca23a174c52a7b7e8a72905661233
jeff Teeters <jeff@teeters.us>
2019-12-16 15:06:19 -0800

    modified speed_check.py to work with installed package and allow command-line arguments for data and java directories

--- speed_check.py
+++ speed_check.py
@@ -8,12 +8,19 @@ import subprocess
 import readline
 import resource
 
+# python_tools_dir = os.getcwd()  # assumes this is running in same directory contaiing this file (speed_check.py)
 
-java_tool_dir = "/Users/jt/crcns/projects/petr_nwbqe_paper/NwbQueryEngine4"
-python_tools_dir = os.getcwd()
-data_dir = "../sample_data"
-java_cmd=("java -Djava.library.path=src/main/resources/" 
-		" -jar target/nwbqueryengine-1.0-SNAPSHOT-jar-with-dependencies.jar " + data_dir)
+
+# default values for command-line arguments
+default_java_tool_dir = "/Users/jt/crcns/projects/petr_nwbqe_paper/NwbQueryEngine4"
+# directory containg nwb files and also the index file (nwb_index.db) built by build_index.py
+default_data_dir = "../sample_data"
+
+# constants
+java_cmd_prefix=("java -Djava.library.path=src/main/resources/" 
+		" -jar target/nwbqueryengine-1.0-SNAPSHOT-jar-with-dependencies.jar ")
+index_file_name = "nwb_index.db"
+cwd = os.getcwd()  # current working directory
 
 
 # Queries currently in paper:
@@ -41,20 +48,35 @@ general/optophysiology/*: (excitation_lambda)
 # *:(neurodata_type == "RoiResponseSeries")
 """
 
-tools = ["java", "search_nwb", "query_index"]
-tool_info = {"java": {"cmd": java_cmd, "dir": java_tool_dir},
-	"search_nwb": {"cmd": "python search_nwb.py " + data_dir, "dir": python_tools_dir},
-	"query_index": {"cmd": "python query_index.py -", "dir": python_tools_dir}
-}
+# tools = ["java", "search_nwb", "query_index"]
+# tool_info = {"java": {"cmd": java_cmd, "dir": java_tool_dir},
+# 	"search_nwb": {"cmd": "python -m nwbindexer.search_nwb " + data_dir, "dir": python_tools_dir},
+# 	"query_index": {"cmd": "python -m nwbindexer.query_index -", "dir": python_tools_dir}
+# }
+
+tools_cmd = None
+tools = None
+def make_tools_cmd(data_dir, index_file_path, java_tool_dir, java_cmd):
+	# creates list of tools and info about tools (command line and directory for running the tool)
+	global java_cmd_prefix, tools_cmd, tools
+	tools_cmd = []
+	if java_tool_dir is not None:
+		tools_cmd.append({"name": "java", "cmd": java_cmd_prefix + data_dir, "dir": java_tool_dir})
+	tools_cmd.append({"name": "search_nwb", "cmd": "python -m nwbindexer.search_nwb " + data_dir})
+	tools_cmd.append({"name": "query_index", "cmd": "python -m nwbindexer.query_index " + index_file_path })
+    # tools has list of tool names
+	tools = [tools_cmd[i]["name"] for i in range(len(tools_cmd))]
+
 
 def run_query(query):
 	# run query on all three tools, returns times as a triple (one for each tool)
-	global tools, tool_info
+	global tools_cmd, cwd
 	times = []
-	for tool in tools:
-		cmd = tool_info[tool]["cmd"] + " '" + query + "'"
-		tdir = tool_info[tool]["dir"]
-		os.chdir(tdir)
+	for tool_info in tools_cmd:
+		tool = tool_info["name"]
+		cmd = tool_info["cmd"] + " '" + query + "'"
+		if "dir" in tool_info and tool_info["dir"] is not None:
+			os.chdir(tool_info["dir"])
 		print("Starting: ** %s ** with: %s" % (tool, query,) )
 		print("dir:%s" % os.getcwd())
 		print("cmd:%s" % cmd)
@@ -75,10 +97,12 @@ def run_query(query):
 			sys.exit("aborting.")
 		print("Time, user={:.4f}, sys={:.4f}, total={:.4f}".format(time_user, time_sys, time_total))
 		times.append(time_total)
+	# change back to original directory in case directory changed
+	os.chdir(cwd)
 	return times
 
 def run_default_queries():
-	global default_queries
+	global default_queries, tools
 	results = []
 	results.append(tools)  # heading of output table
 	queries = []  # for storing actual queries (without blank lines or comments)
@@ -120,16 +144,40 @@ def do_interactive_queries():
 
 
 def display_instructions():
-	print("Usage: %s ( i | - | <query> )" % sys.argv[0])
-	print(" Either one characer: 'i' - interactive mode, '-' use stored default queries")
-	print(" *OR* a single query to execute; must be quoted.")
+	global default_java_tool_dir, default_data_dir, index_file_name
+	print("Usage: %s ( i | - | <query> ) [ <data_dir> [ <java_tool_dir> ] ]" % sys.argv[0])
+	print(" First parameter required:")
+	print("    Either one characer: 'i' - interactive mode, '-' use stored default queries")
+	print("    *OR* a single query to execute; must be quoted.")
+	print(" After the first parameter, optionally specify:")
+	print("    <data_dir> - directory containing nwb files AND index file ('%s' built by build_index.py)" % index_file_name)
+	print("    <java_tool_dir> - directory containing NWB Query Engine (java tool)")
+	print("    If <data_dir> not specified, uses: %s" % default_data_dir)
+	print("    If <java_tool_dir> not specified, uses: %s" % default_java_tool_dir)
 	sys.exit("")
 
 
 def main():
+	global default_java_tool_dir, default_data_dir, index_file_name
 	arglen = len(sys.argv)
-	if arglen != 2:
+	if arglen < 2 or arglen > 4:
 		display_instructions()
+	java_tool_dir = os.path.abspath( argv[3] if arglen == 4 else default_java_tool_dir )
+	data_dir = os.path.abspath( sys.argv[2] if arglen > 2 else default_data_dir )
+	if not os.path.isdir(data_dir):
+		print("ERROR: Data directory not found: '%s'" % data_dir)
+		sys.exit("Aborting.")
+	index_file_path = os.path.join(data_dir, index_file_name)
+	if not os.path.isfile(index_file_path):
+		print("ERROR: Index file not found: '%s'; perhaps need to build it using build_index.py." % index_file_path)
+		sys.exit("Aborting.")
+	if not os.path.isdir(java_tool_dir):
+		print("WARNING: Could not find java_tool_directory: %s" % java_tool_directory)
+		print("Not running Java tool queries")
+		java_cmd = None
+	else:
+		java_cmd = java_cmd_prefix + data_dir
+	make_tools_cmd(data_dir, index_file_path, java_tool_dir, java_cmd)
 	if sys.argv[1] == "i":
 		do_interactive_queries()
 	elif sys.argv[1] == "-":
